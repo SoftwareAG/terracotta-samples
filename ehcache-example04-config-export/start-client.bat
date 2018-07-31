bc9adabed464304a8817bc90fec58e94c4d01302
Venkata Sairam Madduri <VenkataSairam.Madduri@softwareag.com>
2018-07-31 16:06:11 +0530

    Add ehcache3 xml samples

---
+++ ehcache-example04-config-export/start-client.bat
@@ -0,0 +1,54 @@
+@REM Copyright © 2013 - 2018 Software AG, Darmstadt, Germany and/or its licensors
+@REM
+@REM SPDX-License-Identifier: Apache-2.0
+@REM
+@REM   Licensed under the Apache License, Version 2.0 (the "License");
+@REM   you may not use this file except in compliance with the License.
+@REM   You may obtain a copy of the License at
+@REM
+@REM       http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM   Unless required by applicable law or agreed to in writing, software
+@REM   distributed under the License is distributed on an "AS IS" BASIS,
+@REM   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM   See the License for the specific language governing permissions and
+@REM   limitations under the License.
+
+@echo off
+setlocal
+
+set WD=%~d0%~p0
+if not defined TC_HOME (
+  echo Please initialize the environment variable TC_HOME to the location of your extracted TerracottaDB kit
+  pause
+  exit /b 1
+)
+set TC_HOME=%TC_HOME:"=%
+
+if exist "%TC_HOME%\server\bin\setenv.bat" (
+  call "%TC_HOME%\server\bin\setenv.bat"
+)
+
+if not defined JAVA_HOME (
+  echo Environment variable JAVA_HOME needs to be set
+  pause
+  exit /b 1
+)
+
+set JAVA="%JAVA_HOME%\bin\java.exe"
+set JAVAC="%JAVA_HOME%\bin\javac.exe"
+
+set TC_CP=.
+rem Add the client jars in the classpath
+for /F "usebackq delims=" %%I in (`dir /b /s "%TC_HOME%\client\*.jar"`) do call set "TC_CP=%%TC_CP%%;%%I"
+rem Add the logback configuration to the classpath
+set "TC_CP=%TC_CP%;%TC_HOME%\client\logging\impl"
+
+echo Compiling the sample class..
+%JAVAC% -classpath "%TC_CP%" "%WD%\src\EhCache3ProgrammaticConfigExport.java"
+
+echo Starting the ehcache3 sample client, it's going to try to connect to your local server..
+%JAVA% -cp "%TC_CP%;%WD%\src" -Xmx200m EhCache3ProgrammaticConfigExport
+pause
+
+endlocal
---
+++ ehcache-example04-config-export/start-client.sh
@@ -0,0 +1,51 @@
+# Copyright © 2018 Software AG, Darmstadt, Germany and/or its licensors
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+#!/bin/bash
+
+WD=$(cd "$(dirname "$0")";pwd)
+
+if [ -z "$TC_HOME" ]; then
+  echo "Please initialize the environment variable TC_HOME to the location of your extracted TerracottaDB kit"
+  exit 1
+fi
+
+if [ ! -d "${JAVA_HOME}" ]; then
+  echo "$0: the JAVA_HOME environment variable is not defined correctly"
+  exit 2
+fi
+
+JAVA="${JAVA_HOME}/bin/java"
+JAVAC="${JAVA_HOME}/bin/javac"
+
+uname | grep CYGWIN > /dev/null && TC_HOME=$(cygpath -w -p "${TC_HOME}")
+
+TC_CP=.
+# Add the client jars in the classpath
+find "${TC_HOME}/client/" -type f -name "*.jar" > file
+while IFS= read -r line; do
+	TC_CP=${TC_CP}:${line}
+done < file
+rm file
+
+# Add the logback configuration to the classpath
+TC_CP=${TC_CP}:${TC_HOME}/client/logging/impl
+
+echo "Compiling the sample class.."
+"$JAVAC" -classpath "$TC_CP" "${WD}/src/EhCache3ProgrammaticConfigExport.java"
+
+echo "Starting the ehcache3 sample client, it's going to try to connect to your local server.."
+"$JAVA" -Xmx200m -classpath "$TC_CP:$WD/src/" EhCache3ProgrammaticConfigExport
